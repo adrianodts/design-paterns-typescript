20766469ce095aa02582043db3b51795093f24e3
Adriano Dantas <adrianodts@msn.com>
2021-09-18 15:46:55 -0300

    Implementacao do design patern Facade

--- src/structural/adapter/facade/Report.test.ts
+++ src/structural/adapter/facade/Report.test.ts
@@ -4,4 +4,4 @@ test("Should generate a report", () => {
     const reportFacade = new ReportFacade("Header", "Body", "Footer");
     const html = reportFacade.generate();
     expect(html).toBe("<div>Header</div><div>Body</div><div>Footer</div>");
-})
+ });
